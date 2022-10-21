9dc1471ad040ecbaf767bff51b13f5e508cc6327
Daphne <daphneruth304@gmail.com>
2022-10-21 11:49:42 +0300

    updated

--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -2,20 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).render('base', {
     tour: 'The Forest Hiker',
     user: 'Daphne'
   });
 });
 
-app.get('/overview', (req, res) => {
+router.get('/overview', (req, res) => {
   res.status(200).render('base', {
     title: 'All Tours'
   });
 });
-
-app.get('/tour', (req, res) => {
+router.get('/tour', (req, res) => {
   res.status(200).render('base', {
     title: 'The Forest Hiker Tour'
   });
