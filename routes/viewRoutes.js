d7d1549ace588476ed97af118a7f5f3d252d5f91
Daphne <daphneruth304@gmail.com>
2022-10-21 12:08:22 +0300

    updated

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,8 @@
 const express = require('express');
-
+const viewsController = require('../controllers/viewsController');
 const router = express.Router();
 
-router.get('/overview', (req, res) => {
-  res.status(200).render('base', {
-    title: 'All Tours'
-  });
-});
+router.get('/overview', viewsController.overview);
 router.get('/tour', (req, res) => {
   res.status(200).render('base', {
     title: 'The Forest Hiker Tour'
