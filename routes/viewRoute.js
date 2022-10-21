5915b2edc231ec5a5656af997f1925daf1f21caa
Daphne <daphneruth304@gmail.com>
2022-10-21 11:53:29 +0300

    updated

--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -2,13 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Daphne'
-  });
-});
-
 router.get('/overview', (req, res) => {
   res.status(200).render('base', {
     title: 'All Tours'
